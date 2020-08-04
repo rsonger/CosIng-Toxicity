470aed4053228b530a69409a9c8d6067ec431240
Robert Songer <rob@Roberts-MacBook-Pro.local>
2020-08-04 11:42:11 +0900

    Added Top X words to textmining output files

--- textmining.py
+++ textmining.py
@@ -10,17 +10,22 @@ nltk.download('stopwords')
 sentence_detector = nltk.data.load("tokenizers/punkt/english.pickle")
 
 skin_words = ["skin","dermal","epidermis"]
-mystopwords = [".", ",", "(", ")","side","water","bee","target","due","life","%"]
-TOP_WORDS = 10
+mystopwords = [".", ",", "(", ")","side","water","bee","target","due","life","%",";",":"]
+TOP_WORDS = 5
 
 abstractsFolder = "./abstracts/"
 outputFile = "./data/Pubmed_abstracts-cosing+toxicity+skin.csv"
 csvheaders = "Compound Name, Abstract ID, CName Frequency, Skin Freq, Compound-Skin Collocations"
+
+for x in range(TOP_WORDS):
+    csvheaders += ", Top %d, Top %d Freq" % (x,x)
+
 CNAME_COL = 0
 ABSID_COL = 1
 CNAME_FREQ_COL = 2
 SKIN_FREQ_COL = 3
 COLLOCATIONS_COL = 4
+TOP_WORDS_COL = 5
 
 debugFile = "textmining-debug.log"
 errorFile = "textmining-error.log"
@@ -37,7 +42,7 @@ def setupLogging():
     logging.getLogger("").addHandler(errors)
 
 # searches the root folder for TXT files and returns a list of tuples
-# each tuple is (abstract ID, full abstract text, tokenized sentences list)
+# each tuple is (abstract ID, full abstract text, sentence tokens, word tokens)
 def tokenizeAllAbstracts(root):
     abstractWords = []
     for f in os.listdir(root):
@@ -45,10 +50,10 @@ def tokenizeAllAbstracts(root):
         if fileext == ".txt":
             abstractFile = open(root + "/" + f)
             abstractText = abstractFile.read()
-            # words = nltk.word_tokenize(abstractFile.read())
-            # words = [w.lower() for w in words]
+            words = nltk.word_tokenize(abstractText)
+            words = [w.lower() for w in words]
             sentences = sentence_detector.tokenize(abstractText.strip())
-            abstractWords.append((filename, abstractText, sentences))
+            abstractWords.append((filename, abstractText, sentences, words))
     return abstractWords
 
 def countCompoundName(cname, text):
@@ -75,6 +80,20 @@ def getSkinAndCNameCollocations(cname, sentences):
     
     return collocations / len(sentences)
 
+def removeStopWords(tokens):
+    words = []
+    stopwords = nltk.corpus.stopwords.words("english")
+
+    for sw in mystopwords:
+        stopwords.append(sw)
+
+    for w in tokens:
+        if w not in stopwords:
+            words.append(w)
+
+    return words    
+
+
 setupLogging()
 
 outputlist = []
@@ -89,6 +108,17 @@ for f in os.listdir(abstractsFolder):
         row.insert(CNAME_FREQ_COL, countCompoundName(f, abstext))
         row.insert(SKIN_FREQ_COL, countSkinWords(abstext))
         row.insert(COLLOCATIONS_COL, getSkinAndCNameCollocations(f, abstract[2]))
+
+        tkns = removeStopWords(abstract[3])
+        freqDist = nltk.probability.FreqDist(tkns)
+        topwords = freqDist.most_common(TOP_WORDS)
+
+        i = 0
+        for tw in topwords:
+            row.insert(TOP_WORDS_COL + i, tw[0])
+            i += 1
+            row.insert(TOP_WORDS_COL + i, tw[1])
+            i += 1
 
         outputlist.append(row)
 
